fc6e95729e088a2cdbfc5242cd897061ddd79d4b
Isabella <bdc190799@gmail.com>
2023-10-13 19:26:35 +0100

    Created the start of the main logic
    Created the base inquirer logic to be expanded later. The answers will be used in the HTML generator for creating employee cards.

--- index.js
+++ index.js
@@ -12,3 +12,32 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+//gathers the user prompts for the HTML file
+const userPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: "input", //basic text input
+            name: "title",
+            message: "Welcome to the HTML generator. \nTest"
+        },
+        {
+
+        }
+    ])
+    .catch((error) => {
+        if(error.isTtyError)
+        {
+            console.error("Prompt cannot be rendered in current environment, please try again.");
+        }
+        else
+            console.error(`Something went wrong \n + error`);
+    });
+}
+
+const start = async () =>
+{
+    const answers = await userPrompt(); //won't progress until the user finishes their prompts.
+    console.log(answers);
+}
+
+start(); //initialise the script
