51d980aa65ff432362c8f81e5d552b7fda2a5e61
Isabella <bdc190799@gmail.com>
2023-10-17 22:54:41 +0100

    Updated inquirer logic
    Updated the inquirer logic to ask about the manager details, created the start of the menu layout for use when adding additional employees.

--- index.js
+++ index.js
@@ -13,15 +13,50 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 //gathers the user prompts for the HTML file
-const userPrompt = () => {
+const managerPrompt = () => {
     return inquirer.prompt([
         {
             type: "input", //basic text input
-            name: "title",
-            message: "Welcome to the HTML generator. \nTest"
+            name: "managerName",
+            message: "Welcome to the Team generator. \nPlease fill in the following details for the team manager:\n Name: "
         },
         {
+            type: "input",
+            name: "managerID",
+            message: "ID: "
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "Email: "
+        },
+        {
+            type: "input",
+            name: "managerOffice",
+            message: "Office Number: "
+        }
+    ])
+    .catch((error) => {
+        if(error.isTtyError)
+        {
+            console.error("Prompt cannot be rendered in current environment, please try again.");
+        }
+        else
+            console.error(`Something went wrong \n + error`);
+    });
+}
 
+const menuPrompt = () => {
+    return inquirer.prompt([ //overall menu item to be called multiple times.
+        {
+            type: "list",
+            name: "menu",
+            message: "What type of employee would you like to add?",
+            choices: [
+                {name: "Engineer", value: "Engineer"}, //will send user to add engineer entries.
+                {name: "Intern", value: "Intern"},  //will send user to add intern entries.
+                {name: "Finish", value: "Exit"} //will finish the program
+            ]
         }
     ])
     .catch((error) => {
@@ -36,8 +71,16 @@ const userPrompt = () => {
 
 const start = async () =>
 {
-    const answers = await userPrompt(); //won't progress until the user finishes their prompts.
-    console.log(answers);
+    const managerAnswers = await managerPrompt(); //won't progress until the user finishes their prompts.
+    console.log(managerAnswers);
+    let needsEmployees = true; //will loop the following answer getter until the user no longer needs employees.
+    let engineerAnswers = []; //array of engineers in the team
+    let internAnswers = []; //array of interns in the team
+
+    while(needsEmployees) //will continue to ask the user what they would like to add, or if they would like to finish
+    {
+        if()
+    }
 }
 
 start(); //initialise the script
